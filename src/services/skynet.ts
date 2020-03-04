83d8a2276fe97739f034338300c93f83c46c6c3a
Leroy Shirto <leroy.shirto@fasthosts.com>
2020-03-04 12:28:45 +0000

    Download progress bar

--- src/services/skynet.ts
+++ src/services/skynet.ts
@@ -1,3 +1,4 @@
+import axios from 'axios';
 import shortid from 'short-id';
 
 type Meta = {
@@ -10,7 +11,7 @@ export type SkynetFile = {
   data: Blob;
 }
 
-class SkynetClient {
+export class SkynetClient {
   private PORTAL_URL = 'https://skynet.tutemwesi.com';
   // private PORTAL_URL = 'https://siasky.net';
 
@@ -19,6 +20,8 @@ class SkynetClient {
   private PORTAL_FILE_FIELD_NAME = 'file';
 
   private SKYNET_FILE_META_HEADER = 'skynet-file-metadata';
+
+  static SKYNET_DOWNLOAD_PROGRESS_EVENT = 'skynet-download-progress'
 
   async uploadFile(file: File) {
     const data = new FormData();
@@ -30,22 +33,46 @@ class SkynetClient {
       { method: 'POST', body: data },
     );
 
+    // const response = await axios.request(
+    //   {
+    //     method: 'post',
+    //     url: `${this.PORTAL_URL}/${this.PORTAL_UPLOAD_PATH}/${uuid}`,
+    //     data,
+    //     onUploadProgress: (p) => {
+    //       console.log(p);
+    //     },
+    //   },
+    // );
+
     return response.json();
   }
 
   async downloadFile(skylink: string) {
-    const response = await fetch(`${this.PORTAL_URL}/${skylink}`);
-    if (!response.ok) {
+    const response = await axios.request(
+      {
+        method: 'get',
+        url: `${this.PORTAL_URL}/${skylink}`,
+        responseType: 'blob', // important
+        onDownloadProgress: (p: ProgressEvent) => {
+          const eventAwesome = new CustomEvent(SkynetClient.SKYNET_DOWNLOAD_PROGRESS_EVENT, {
+            bubbles: true,
+            detail: p,
+          });
+          window.dispatchEvent(eventAwesome);
+        },
+      },
+    );
+    if (response.status !== 200) {
       throw new Error(response.statusText);
     }
 
-    if (!response.headers.has(this.SKYNET_FILE_META_HEADER)) {
+    if (!response.headers[this.SKYNET_FILE_META_HEADER]) {
       throw new Error('Could not get the links meta data');
     }
 
     // {"filename":"test.html"}
-    const meta = JSON.parse(response.headers.get('skynet-file-metadata') as string);
-    const data = await response.blob();
+    const meta = JSON.parse(response.headers['skynet-file-metadata'] as string);
+    const data = await response.data;
 
     return { meta, data };
   }
--- src/views/Download.vue
+++ src/views/Download.vue
@@ -1,26 +1,43 @@
 <template>
-  <section>
-    <div class="container">
-      <div v-show="loading" class="text-center">
-        <b-spinner variant="primary" label="Spinning"></b-spinner>
+  <section class="container text-center  mt-30">
+    <b-card>
+      <div v-show="loading">
+        <h5>Loading file...</h5>
+        <b-progress
+          :value="downloadProgress.loaded"
+          :max="downloadProgress.total"
+          show-progress animated>
+        </b-progress>
       </div>
-      <div v-if="downloadedFile !== null || downloadedFile !== undefined">
+      <div class="mt-30" v-if="downloadedFile !== null || downloadedFile !== undefined">
         <p v-if="downloadedFile !== null">This file is: {{downloadedFile.meta.filename}}</p>
         <b-btn @click="forceFileDownload(downloadedFile)">Download</b-btn>
       </div>
-    </div>
+    </b-card>
   </section>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
-import getClient, { SkynetFile } from '@/services/skynet';
+import getClient, { SkynetFile, SkynetClient } from '@/services/skynet';
 
 @Component({ components: {} })
 export default class Home extends Vue {
   private loading = true;
 
+  private downloadProgress = {
+    loaded: 0,
+    total: 0,
+  };
+
+  private skynetClient: SkynetClient;
+
   private downloadedFile: SkynetFile | null = null;
+
+  constructor() {
+    super();
+    this.skynetClient = getClient();
+  }
 
   async created() {
     const { skylink } = this.$route.params;
@@ -29,12 +46,16 @@ export default class Home extends Vue {
       this.$swal('Cloud not find skylink');
     }
 
+    window.addEventListener(SkynetClient.SKYNET_DOWNLOAD_PROGRESS_EVENT, (e: Event) => {
+      const downloadStatsEvent = e as CustomEvent;
+      this.downloadProgress.loaded = downloadStatsEvent.detail.loaded;
+      this.downloadProgress.total = downloadStatsEvent.detail.total;
+    });
     this.downloadedFile = await this.fetchLink(skylink);
   }
 
   async fetchLink(skylink: string) {
-    const client = getClient();
-    const response = await client.downloadFile(skylink);
+    const response = await this.skynetClient.downloadFile(skylink);
 
     this.loading = false;
 
@@ -55,5 +76,20 @@ export default class Home extends Vue {
 <style scoped lang="scss">
 .mt-20 {
   margin-top: 20px;
+}
+
+.mt-30 {
+  margin-top: 30px;
+}
+
+.card {
+  background: #fff;
+  border: none;
+  box-shadow: 0px 5px 42px 0px rgba(35, 70, 107, 0.08);
+  border-radius: 0.8rem;
+}
+
+.card:hover {
+  box-shadow: 0px 25px 42px 0px rgba(35, 70, 107, 0.24);
 }
 </style>

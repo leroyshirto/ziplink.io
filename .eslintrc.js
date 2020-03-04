a0fc1c0a567c0537e6f29bf2112509bb93514d90
Leroy Shirto <leroyshirto@gmail.com>
2020-03-04 00:42:11 +0000

    allow console loggin in prod

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,7 +12,7 @@ module.exports = {
     ecmaVersion: 2020,
   },
   rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'error',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'class-methods-use-this': 'off',
     'import/no-extraneous-dependencies': 'off',

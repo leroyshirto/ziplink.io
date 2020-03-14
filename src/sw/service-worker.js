cd1ac72c52caf47dad354d31c4b3d7f64ded674e
Leroy Shirto <leroyshirto@gmail.com>
2020-03-14 12:26:19 +0000

    cant use import in the service worker

--- src/sw/service-worker.js
+++ src/sw/service-worker.js
@@ -1,5 +1,4 @@
 /* eslint-disable no-restricted-globals,no-underscore-dangle */
-import { serveShareTarget } from './util';
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
  * requests for URLs in the manifest.
@@ -8,6 +7,43 @@ import { serveShareTarget } from './util';
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 // eslint-disable-next-line no-undef
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+// Utils
+function serveShareTarget(event) {
+  const dataPromise = event.request.formData();
+
+  // Redirect so the user can refresh the page without resending data.
+  event.respondWith(Response.redirect('/#/?upload'));
+
+  event.waitUntil(async function () {
+    // The page sends this message to tell the service worker it's ready to receive the file.
+    // eslint-disable-next-line @typescript-eslint/no-use-before-define
+    await nextMessage('share-ready');
+    // eslint-disable-next-line no-restricted-globals
+    const client = await self.clients.get(event.resultingClientId);
+    const data = await dataPromise;
+    const file = data.get('file');
+    client.postMessage({ file, action: 'load-image' });
+  }());
+}
+
+const nextMessageResolveMap = {};
+
+/**
+ * Wait on a message with a particular event.data value.
+ *
+ * @param dataVal The event.data value.
+ */
+function nextMessage(dataVal) {
+  return new Promise((resolve) => {
+    if (!nextMessageResolveMap.has(dataVal)) {
+      nextMessageResolveMap.set(dataVal, []);
+    }
+    // eslint-disable-next-line no-unused-expressions
+    nextMessageResolveMap.get(dataVal)?.push(resolve);
+  });
+}
+
 
 // Install new service worker when ok then reload the page
 self.addEventListener('message', (msg) => {
